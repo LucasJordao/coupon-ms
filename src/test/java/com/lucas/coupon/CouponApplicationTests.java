0ed72e4f54f26fb1fe17e46202e39e7ca9042c17
LucasJordao <lucaswill12@hotmail.com>
2026-01-02 21:52:29 -0300

    feat: init commit

---
+++ src/test/java/com/lucas/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
